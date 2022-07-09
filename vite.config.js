720a796e9d5d6985c229ef1cc5211fb01cb0312c
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2022-07-09 07:26:58 +0000

    Updated vite config to run on codespaces.

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
     },
     plugins: [babel(), reactRefresh()],
     server: {
+        hmr: { clientPort: process.env.CODESPACES ? 443 : undefined },
         host: true,
     },
 })

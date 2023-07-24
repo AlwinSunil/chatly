b65bb1dabd6eaff8327573bcfda1723c84c4e4ef
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2023-07-24 14:24:17 +0530

    Improved about page.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-export default function handler(response) {
-    response
+export default function handler(request, response) {
+    return response
         .status(200)
         .send(
-            "Welocome to Chatly's Internal API. I think you shouldn't be here.<br>Visit <a href=https://chatly.alwinsunil.in/ target=_blank rel=noopener noreferrer> chatly.alwinsunil.in<a/>"
+            "Welocome to Chatly's Internal API. I think you shouldn't be here.<br>Visit <a href=https://chatly.alwinsunil.in/ target=_blank rel=noopener noreferrer> chatly.alwinsunil.in<a/>" +
+                `${request.query}`
         )
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,18 +4,39 @@ import styles from "./About.module.scss"
 
 function About() {
     return (
-        <div>
+        <>
             <Navigation header="About" />
             <div className={styles.about}>
                 <div className={styles.header}>
-                    <div className={styles.branding}>
-                        <img src="/assets/icons/logo.svg" alt="" />
-                        <span className={styles.wordmark}>Chatly</span>
-                        <p>v 1.0.0</p>
-                    </div>
+                    <img src="/assets/icons/logo.svg" alt="" />
+                    <span className={styles.wordmark}>Chatly</span>
+                    <p>v 1.0.0</p>
+                </div>
+                <div className={styles.details}>
+                    <a
+                        className={`${styles.button} menu`}
+                        href="http://gihub.com/AlwinSunil/chatly"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <p>
+                            Github repo
+                            <span>github.com/AlwinSunil/chatly</span>
+                        </p>
+                    </a>
+                    <p className={styles.author}>
+                        Project by:&nbsp;
+                        <a
+                            href="http://gihub.com/AlwinSunil"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Alwin Sunil
+                        </a>
+                    </p>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
